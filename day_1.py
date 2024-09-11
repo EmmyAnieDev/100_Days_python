a27c288c2bc6c3d114340347832008c3119b0940
EmmyAnieDev <Emaduilzjr1@gmail.com>
2024-09-11 21:59:37 +0100

    day 1

---
+++ day_1.py
@@ -0,0 +1,23 @@
+print('Hello Good day\nType your name below')
+
+name = input('')
+
+print(f'your name is {name}')
+
+
+# 2
+occupation = input('what do you do? ')
+
+print('so your occupation is a', occupation)
+
+
+length_of_occupation_letters = len(occupation)
+print(length_of_occupation_letters)
+
+
+# 3   Band Name Generator
+print('Welcome to the Band Name Generator')
+age = input('what\s your age ')
+height = input('what\s your height in meters ')
+
+print(f'your age and height are {age} and {height}m respectively.')
